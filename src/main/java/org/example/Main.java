12577a8073675f5dbd6d6d69bfbfc9f25c24d670
Kamil Witczak <kamilgimp99@gmail.com>
2023-03-04 17:58:21 +0100

    Configuration of JavaFx

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        GUI gui = new GUI();
     }
 }
